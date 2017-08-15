d99ca5aea6921d9cd9c0f56ce23c733e8178ed5a
Dalon Westergreen <dalon.westergreen@intel.com>
2017-08-14 20:38:28 -0700

    Update release script
     -> include base and mandelbrot designs
     -> releases should be chronological, update Tag format
     -> Add archive in release script
    
    Signed-off-by: Dalon Westergreen <dalon.westergreen@intel.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -22,16 +22,18 @@
 
 #!/bin/bash
 
-STAMP=$(date -u +"%m-%d-%y_%H.%M.%S")
+# OLD STAMP STAMP=$(date -u +"%m-%d-%y_%H.%M.%S")
+STAMP=$(date -u +"%Y%m%d_%H.%M.%S")
+STAMP_SHORT=$(date -u +"%Y%m%d")
 QUART_VER=$(head -1 $QUARTUS_ROOTDIR/version.txt)
 
 # tag master
-git tag -s -a release-$STAMP -m "Release:$STAMP"
+git tag -s -a RELEASE-$STAMP -m "Release Build:$STAMP QUARTUS:$QUART_VER"
 
 git checkout --orphan temp-RELEASE-BUILD-$STAMP
 git rm -rf --cached .
 
-for i in de0-nano de10-nano; do
+for i in de10-nano-fft de10-nano-base de10-nano-mandelbrot; do
 	git add $i/hps_isw_handoff/*
 	git add $i/preloader/*
 	git add $i/output_files/*.rbf
@@ -44,13 +46,16 @@ for i in de0-nano de10-nano; do
 done
 
 git add LICENSE
-git add devicetrees/*.dtso
-git add devicetrees/*.dtbo
-git add devicetrees/*.dtsi
+git add README.md
+git add Makefile
+
+git add devicetrees/*
+git add mks/*
+git add scripts/*
+git add utils/*
 
 git add ip/*
 git add hdl_src/*
-
 git add patches/sopc2dts/* 
 
 
@@ -63,38 +68,5 @@ sed -i '/^\.\/$/d' $STAMP.filelist
 git add $STAMP.filelist
 git commit -a -s -S -m "Build:$STAMP QUARTUS:$QUART_VER"
 
-git checkout -f RELEASE_BUILDS
-
-git rm -rf .
-
-git checkout temp-RELEASE-BUILD-$STAMP $STAMP.filelist;
-for i in $(cat $STAMP.filelist); do
-	git checkout temp-RELEASE-BUILD-$STAMP $i;
-done
-
-git add $STAMP.filelist
-git add LICENSE
-git add devicetrees/*.dtso
-git add devicetrees/*.dtbo
-git add devicetrees/*.dtsi
-
-git add ip/*
-git add hdl_src/*
-
-git add patches/sopc2dts/* 
-
-for i in de0-nano de10-nano; do
-        git add $i/hps_isw_handoff/*
-        git add $i/preloader/*
-        git add $i/output_files/*.rbf
-        git add $i/output_files/*.sof
-	git add $i/*.qpf
-	git add $i/*.qsf
-	git add $i/*.qsys
-	git add $i/*.sopcinfo
-	git add $i/*.ipx
-done
-
-git commit -a -s -S -m "Build:$STAMP QUARTUS:$QUART_VER"
 git tag -s -a RELEASE-BUILD-$STAMP -m "Build:$STAMP QUARTUS:$QUART_VER"
-
+git archive --format=tar.gz --prefix=de10-nano-build_$STAMP_SHORT/ --output=de10-nano-build_$STAMP_SHORT.tgz RELEASE-BUILD-$STAMP
